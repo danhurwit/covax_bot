fd8dffbf4354be2c3634040d1353caea43e3b75d
danhurwit <dhurwit@gmail.com>
2021-03-04 19:12:15 -0500

    remove url

--- src/publisher/channel/DiscordPublisher.py
+++ src/publisher/channel/DiscordPublisher.py
@@ -1,17 +1,18 @@
+import os
 from typing import List
 
 from publisher.channel.Publisher import Publisher
 from scraper.models.Location import Location
 
 from discord import Webhook, RequestsWebhookAdapter
-
-
-DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/817171915331862588/pZ2gbKgfRfdTI_IdPNtu-uBezD4w2u6VjA59govva8AM3PYJFOm_TwZsP4MAbj171PUL'
+from decouple import config
 
 
 class DiscordPublisher(Publisher):
 
+    publish_url = config('DISCORD_WEBHOOK_URL')
+
     def publish(self, locations: List[Location]):
         for location in locations:
-            webhook = Webhook.from_url(DISCORD_WEBHOOK_URL, adapter=RequestsWebhookAdapter())
+            webhook = Webhook.from_url(self.publish_url, adapter=RequestsWebhookAdapter())
             webhook.send(location.format_message())
