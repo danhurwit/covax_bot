8721a5ee648c1111e74af59b7d09b49e6d83477b
danhurwit <dhurwit@gmail.com>
2021-03-21 19:56:00 -0400

    try again

--- src/scraper/sources/Walgreens.py
+++ src/scraper/sources/Walgreens.py
@@ -1,6 +1,7 @@
+import logging
 from datetime import datetime
+from pprint import pprint
 
-import pprint
 from requests import Session
 
 from models.sources.AppointmentSource import AppointmentSource
@@ -11,7 +12,7 @@ from models.sources.Location import Location
 
 class Walgreens(AppointmentSource):
     name = "Walgreens"
-    scrape_url = 'https://www.walgreens.com/hcschedulersvc/svc/v1/immunizationLocations/availability/'
+    scrape_url = 'https://www.walgreens.com/hcschedulersvc/svc/v1/immunizationLocations/availability'
     __request_payload = {"serviceId": "99",
                          "position": {"latitude": 42.36475590000001, "longitude": -71.1032591},
                          "appointmentAvailability": {"startDateTime": "2021-03-22"},
@@ -22,11 +23,13 @@ class Walgreens(AppointmentSource):
         "3CB371")
 
     def scrape_locations(self):
+        logging.basicConfig(level=logging.DEBUG)
         session, token = self.__get_session()
         session.headers.update({'x-xsrf-token': token})
         response = session.post(url=self.scrape_url,
                                 json=self.__request_payload)
         locations = []
+        pprint(response.request.headers)
         if response.json()['appointmentsAvailable']:
             locations.append(Location(self.name,
                                       self.get_global_booking_link(),
@@ -36,6 +39,7 @@ class Walgreens(AppointmentSource):
 
     def __get_session(self):
         s = Session()
+        s.headers.update({"user-agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36'})
         s.get('https://www.walgreens.com/topic/promotion/covid-vaccine.jsp')
         csrf_response = s.get('https://www.walgreens.com/topic/v1/csrf')
         s.get('https://www.walgreens.com/findcare/vaccination/covid-19?ban=covid_vaccine_landing_schedule')
