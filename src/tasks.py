36866dbfb21c8e9ef998cd7d3222ce018e3984a4
danhurwit <dhurwit@gmail.com>
2021-03-27 14:49:02 -0400

    every 2 minutes

--- src/tasks.py
+++ src/tasks.py
@@ -5,7 +5,7 @@ from .celery_app import app
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls run() every 60 seconds
-    sender.add_periodic_task(60.0, scrape_appointments.s(), expires=10)
+    sender.add_periodic_task(120.0, scrape_appointments.s(), expires=10)
 
 
 @app.task
