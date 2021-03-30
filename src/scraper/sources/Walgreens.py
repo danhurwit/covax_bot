abeb62dcdd60aa7c4840e69b95c5332bef584572
danhurwit <dhurwit@gmail.com>
2021-03-29 21:43:25 -0400

    more humanlike

--- src/scraper/sources/Walgreens.py
+++ src/scraper/sources/Walgreens.py
@@ -19,41 +19,22 @@ from models.sources.Location import Location
 class Walgreens(AppointmentSource):
     name = "Walgreens"
     scrape_url = 'https://www.walgreens.com/findcare/vaccination/covid-19?ban=covid_vaccine_landing_schedule'
-    global_booking_link = 'https://www.walgreens.com/topic/promotion/covid-vaccine.jsp'
+    global_booking_link = 'https://www.walgreens.com/findcare/vaccination/covid-19/'
     display_properties = DisplayProperties(
         "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Walgreens_Logo.svg/2560px-Walgreens_Logo.svg.png",
         "3CB371")
 
     def scrape_locations(self):
         chrome_options = Options()
-        chrome_options.headless = True # detected in headless mode
+        chrome_options.headless = True  # detected in headless mode
         driver = webdriver.Chrome(executable_path=config('DRIVER_PATH'), options=chrome_options)
         try:
-            driver.get(self.scrape_url)
-            button = driver.find_element_by_link_text("Schedule new appointment")
-            time.sleep(random.randint(0, 2))
-            webdriver.ActionChains(driver).move_to_element(button).perform()
-            time.sleep(random.randint(0, 2))
-            webdriver.ActionChains(driver).click_and_hold(button).perform()
-            time.sleep(random.randint(0, 2))
-            webdriver.ActionChains(driver).release(button).perform()
-            button = WebDriverWait(driver, 10) \
-                .until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                                   "#wag-body-main-container > section "
-                                                   "> section > section > section > "
-                                                   "section.LocationSearch_container.mt25 > div > span > button")))
-            time.sleep(random.randint(1, 6))
-            button.click()
-            appointments_available = None
-            try:
-                appointments_available = WebDriverWait(driver, 10) \
-                    .until(EC.visibility_of_element_located((By.CSS_SELECTOR,
-                                                             "#wag-body-main-container > section > section > section > "
-                                                             "section > section.eligibleScreens > section > div > a > "
-                                                             "span:nth-child(2) > p"))).text
-            except Exception as e:
-                logger.log("no appointments this time")
-
+            self.nav_to_location_screening(driver)
+            search_button = self.wait_for_search_button(driver)
+            self.random_sleep(2000, 5000, 500)
+            self.enter_zip_code(driver)
+            self.click_random_interval(search_button, driver)
+            appointments_available = self.validate_availability(driver)
             if appointments_available:
                 self.locations = [Location(self.name,
                                            self.get_global_booking_link(),
@@ -63,3 +44,57 @@ class Walgreens(AppointmentSource):
             logger.log("Walgreens scrape error\n {}".format(driver.page_source[:1000]))
         finally:
             driver.close()
+
+    def wait_for_search_button(self, driver):
+        button = WebDriverWait(driver, 10) \
+            .until(EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                               "#wag-body-main-container > section "
+                                               "> section > section > section > "
+                                               "section.LocationSearch_container.mt25 > div > span > button")))
+        return button
+
+    def nav_to_location_screening(self, driver):
+        driver.get(self.scrape_url)
+        redirect_button = driver.find_element_by_link_text("Schedule new appointment")
+        self.random_sleep(1000, 3000, 250)
+        self.click_random_interval(redirect_button, driver)
+
+    def enter_zip_code(self, driver):
+        input_element = driver.find_element_by_id("inputLocation")
+        self.random_sleep(100, 500, 50)
+        input_element.clear()
+        self.random_sleep(300, 500, 50)
+        input_element.send_keys("0")
+        self.random_sleep(10, 50, 5)
+        input_element.send_keys("2")
+        self.random_sleep(10, 50, 5)
+        input_element.send_keys("1")
+        self.random_sleep(10, 50, 5)
+        input_element.send_keys("3")
+        self.random_sleep(10, 50, 5)
+        input_element.send_keys("9")
+
+    def validate_availability(self, driver):
+        appointments_available = None
+        try:
+            appointments_available = WebDriverWait(driver, 3) \
+                .until(EC.visibility_of_element_located((By.CSS_SELECTOR,
+                                                         "#wag-body-main-container > section > section > section > "
+                                                         "section > section.eligibleScreens > section > div > a > "
+                                                         "span:nth-child(2) > p"))).text
+        except Exception as e:
+            print("no appointments this time")
+        return appointments_available
+
+    def click_random_interval(self, button, driver):
+        webdriver.ActionChains(driver).move_to_element(button).perform()
+        self.random_sleep(100, 1000, 50)
+        webdriver.ActionChains(driver).click_and_hold(button).perform()
+        self.random_sleep(100, 1000, 50)
+        webdriver.ActionChains(driver).release(button).perform()
+
+    def random_sleep(self, lower_limit_millis, upper_limit_millis, noise):
+        lower_jitter = random.randint(lower_limit_millis - noise, lower_limit_millis + noise)
+        upper_jitter = random.randint(upper_limit_millis - noise, upper_limit_millis + noise)
+        rand = random.randint(lower_jitter, upper_jitter) / 1000
+        time.sleep(rand)
